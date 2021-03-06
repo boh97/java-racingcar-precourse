feb8337de975cc03e801c66540e4e7d82abcb250
BohGyu <qhrb1997@naver.com>
2021-03-06 18:42:43 +0900

    feat : io

--- src/main/java/io/InputView.java
+++ src/main/java/io/InputView.java
@@ -1,8 +1,60 @@
 package io;
 
+import controller.InputExceptionController;
+import racingcar.Car;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 분리됩니다.";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇회인가요?";
 
-    public static
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() { }
+
+    public static String getStringCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+
+        return scanner.nextLine();
+    }
+
+    public static String[] getValidatedCarNameArray() {
+        String[] carNameArray = getStringCarNames().split(",");
+
+        while (InputExceptionController.isDuplicatedName(carNameArray)
+                || !InputExceptionController.isValidatedName(carNameArray)) {
+            getValidatedCarNameArray();
+        }
+
+        return carNameArray;
+    }
+
+    public static List<Car> getValidatedCarList() {
+        List<Car> carList = new ArrayList<>();
+
+        for (String carName : getValidatedCarNameArray()) {
+            carList.add(new Car(carName));
+        }
+
+        return carList;
+    }
+
+    public static String getTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+
+        return scanner.next();
+    }
+
+    public static int getValidatedTryCount() {
+        String tryCount = getTryCount();
+
+        while (!InputExceptionController.isValidatedTryCount(tryCount)) {
+            getValidatedTryCount();
+        }
+
+        return Integer.parseInt(tryCount);
+    }
 }
--- src/main/java/io/OutputView.java
+++ src/main/java/io/OutputView.java
@@ -1,4 +1,46 @@
 package io;
 
+import controller.MovementController;
+import controller.RankController;
+import jdk.internal.joptsimple.internal.Strings;
+import racingcar.Car;
+
+import java.util.List;
+
 public class OutputView {
+    private static final String EXECUTE_RESULT = "실행결과";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자: ";
+
+    private OutputView() { }
+
+    public static void printSectionResult(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.print(car.getName() + " : ");
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print('-');
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printWholeResult(List<Car> carList, int tryCount) {
+        System.out.println(EXECUTE_RESULT);
+
+        for (int i = 0; i < tryCount; i++) {
+            MovementController.moveOrNot(carList);
+            printSectionResult(carList);
+        }
+    }
+
+    public static void printRankResult(List<Car> carList) {
+        List<Car> winnerList = RankController.getWinnerList(carList);
+
+        System.out.print(FINAL_WINNER_MESSAGE);
+        for (int i = 0; i < winnerList.size() - 1; i++) {
+            System.out.print(winnerList.get(i) + ", ");
+        }
+
+        System.out.println(winnerList.get(winnerList.size() - 1));
+    }
 }
